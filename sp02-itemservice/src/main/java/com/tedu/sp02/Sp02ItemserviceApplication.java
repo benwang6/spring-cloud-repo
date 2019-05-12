8536e5f417e54c5983ef374dcf367e04f6397d11
benwang <benwang@163.com>
2019-05-12 22:06:07 +0800

    8

--- sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
@@ -2,7 +2,9 @@ package com.tedu.sp02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Sp02ItemserviceApplication {
 
