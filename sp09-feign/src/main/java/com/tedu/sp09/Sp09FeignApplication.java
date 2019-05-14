fe7fb8c3660e2c3d3c7f776f2d02d9334a3d2c86
benwang <benwang@163.com>
2019-05-14 22:37:57 +0800

    17

--- sp09-feign/src/main/java/com/tedu/sp09/Sp09FeignApplication.java
+++ sp09-feign/src/main/java/com/tedu/sp09/Sp09FeignApplication.java
@@ -2,9 +2,11 @@ package com.tedu.sp09;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
