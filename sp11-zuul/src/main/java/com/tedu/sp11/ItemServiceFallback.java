4492b8f4e2a96b538422f73a5453c71dfb08f5b2
benwang <benwang@163.com>
2019-05-21 22:18:20 +0800

    20

---
+++ sp11-zuul/src/main/java/com/tedu/sp11/ItemServiceFallback.java
@@ -0,0 +1,65 @@
+package com.tedu.sp11;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import com.tedu.web.util.JsonResult;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ItemServiceFallback implements FallbackProvider {
+	@Override
+	public String getRoute() {
+		return "item-service"; //"*"; //null;
+	}
+
+	@Override
+	public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return response();
+	}
+
+	private ClientHttpResponse response() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.OK.value();
+            }
+            @Override
+            public String getStatusText() throws IOException {
+                return HttpStatus.OK.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+            	log.info("fallback body");
+            	String s = JsonResult.err().msg("后台服务错误").toString();
+                return new ByteArrayInputStream(s.getBytes("UTF-8"));
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
