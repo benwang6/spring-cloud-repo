3878f55788b1fb61587fa878ff10182491fff2e4
benwang <benwang@163.com>
2019-05-21 22:53:59 +0800

    23

--- sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.tedu.sp03.user.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -12,9 +13,12 @@ import com.tedu.web.util.JsonUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RefreshScope
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+
+    //动态刷新时，更新的配置数据，会重新注入
 	@Value("${sp.user-service.users}")
 	private String userJson;
 	
---
+++ sp12-config/src/test/java/com/tedu/sp12/Sp12ConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tedu.sp12;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sp12ConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
