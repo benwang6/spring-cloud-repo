7db3e60a3ff5778cc71cf0994185a61b9783fcc8
benwang6 <45354575+benwang6@users.noreply.github.com>
2019-12-04 22:43:56 +0800

    discovery client

--- sp03-userservice/src/main/java/cn/tedu/sp03/Sp03UserserviceApplication.java
+++ sp03-userservice/src/main/java/cn/tedu/sp03/Sp03UserserviceApplication.java
@@ -2,7 +2,9 @@ package cn.tedu.sp03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Sp03UserserviceApplication {
 
--- sp04-orderservice/src/main/java/cn/tedu/sp04/Sp04OrderserviceApplication.java
+++ sp04-orderservice/src/main/java/cn/tedu/sp04/Sp04OrderserviceApplication.java
@@ -2,7 +2,9 @@ package cn.tedu.sp04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Sp04OrderserviceApplication {
 
