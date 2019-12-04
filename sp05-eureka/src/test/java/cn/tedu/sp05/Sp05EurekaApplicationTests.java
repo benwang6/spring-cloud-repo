08812c581080815625fd59fe87dc2c459574da74
benwang6 <45354575+benwang6@users.noreply.github.com>
2019-12-04 22:09:20 +0800

    第7章

---
+++ sp05-eureka/src/test/java/cn/tedu/sp05/Sp05EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp05EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
