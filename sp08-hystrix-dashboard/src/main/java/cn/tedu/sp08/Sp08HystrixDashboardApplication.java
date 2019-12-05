f9b31bd3a5451ca75a9df139daed053fc97cb83f
benwang6 <45354575+benwang6@users.noreply.github.com>
2019-12-05 23:44:38 +0800

    第13章

---
+++ sp08-hystrix-dashboard/src/main/java/cn/tedu/sp08/Sp08HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package cn.tedu.sp08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class Sp08HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp08HystrixDashboardApplication.class, args);
+	}
+
+}
---
+++ sp08-hystrix-dashboard/src/test/java/cn/tedu/sp08/Sp08HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp08HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
