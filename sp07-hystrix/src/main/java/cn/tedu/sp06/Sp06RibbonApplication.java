183fe92a5edf41dbe488cc8d1125b421ea91e1da
benwang6 <45354575+benwang6@users.noreply.github.com>
2019-12-05 22:52:10 +0800

    第12章

---
+++ sp07-hystrix/src/main/java/cn/tedu/sp06/Sp06RibbonApplication.java
@@ -0,0 +1,33 @@
+package cn.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+//@EnableCircuitBreaker
+//@EnableDiscoveryClient
+//@SpringBootApplication
+
+@SpringCloudApplication
+public class Sp06RibbonApplication {
+	
+	//创建 RestTemplate 实例，并存入 spring 容器
+	@LoadBalanced //负载均衡注解
+	@Bean
+	public RestTemplate getRestTemplate() {
+		SimpleClientHttpRequestFactory f = new SimpleClientHttpRequestFactory();
+		f.setConnectTimeout(1000);
+		f.setReadTimeout(1000);
+		return new RestTemplate(f);
+		
+		//return new RestTemplate();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Sp06RibbonApplication.class, args);
+	}
+
+}
---
+++ sp07-hystrix/src/test/java/cn/tedu/sp06/Sp06RibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp06RibbonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
