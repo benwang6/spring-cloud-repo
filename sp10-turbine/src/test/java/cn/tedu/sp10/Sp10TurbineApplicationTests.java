90f526aabbdc6a8dc2319227fc374625ebcc4eee
benwang6 <45354575+benwang6@users.noreply.github.com>
2019-12-07 22:59:09 +0800

    第19章

---
+++ sp10-turbine/src/test/java/cn/tedu/sp10/Sp10TurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp10TurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
