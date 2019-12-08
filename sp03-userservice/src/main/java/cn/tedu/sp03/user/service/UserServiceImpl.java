3adb9db97e28045400b4bccc3cd9132d94ff73c1
benwang6 <45354575+benwang6@users.noreply.github.com>
2019-12-08 23:47:13 +0800

    第23章

--- sp03-userservice/src/main/java/cn/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/cn/tedu/sp03/user/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package cn.tedu.sp03.user.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -12,6 +13,7 @@ import cn.tedu.web.util.JsonUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RefreshScope
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
